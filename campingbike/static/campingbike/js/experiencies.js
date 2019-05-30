3990d1e2b4a5a57359e473f7c39af110175b5557
zyro <zyro7@hotmail.com>
2019-05-30 16:16:24 +0200

    seguim ordenan codi

---
+++ campingbike/static/campingbike/js/experiencies.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+        $('.lazy').Lazy({
+        // your configuration goes here
+        scrollDirection: 'vertical',
+        effect: 'fadeIn',
+        visibleOnly: true,
+        onError: function(element) {
+            console.log('error loading ' + element.data('src'));
+        }
+    });
+        $('.parallax-window').parallax();
+    });
